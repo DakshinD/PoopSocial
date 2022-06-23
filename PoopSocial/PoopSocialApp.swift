3137b6cbbce79838c429eabee31e1b2a7ba6b405
DakshinD <dakshind2005@gmail.com>
2022-06-23 10:08:45 -0500

    Initial Commit

---
+++ PoopSocial/PoopSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoopSocialApp.swift
+//  PoopSocial
+//
+//  Created by Dakshin Devanand on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct PoopSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
